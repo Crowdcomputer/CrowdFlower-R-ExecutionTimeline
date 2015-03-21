9b40e776cd83accafc5b1dbd84c861fe1fe38371
Pavel Kucherbaev <pavel.kucherbaev@gmail.com>
2015-03-21 16:08:19 +0100

    plot example added conditional download added

--- exectime.R
+++ exectime.R
@@ -11,10 +11,11 @@ source("crowdflower_tasks_performance.R")
 # 2015-03-19
 # 14:00, 16:00, 20:00
 #####################################################################
+to_download = FALSE
 
-batch1 <- processBatch(c(704588,704586,704587), "12:00", "Streaming",40)
-batch2 <- processBatch(c(704621,704623,704622), "16:00", "Streaming",40)
-batch3 <- processBatch(c(704730,704733,704731), "20:00", "Streaming",40)
+batch1 <- processBatch(c(704588,704586,704587), "12:00", "Streaming",40,to_download)
+batch2 <- processBatch(c(704621,704623,704622), "16:00", "Streaming",40,to_download)
+batch3 <- processBatch(c(704730,704733,704731), "20:00", "Streaming",40,to_download)
 
 batches_with_streaming <- rbind(batch1, batch2, batch3)
 #####################################################################
@@ -23,9 +24,9 @@ batches_with_streaming <- rbind(batch1, batch2, batch3)
 # 14:00, 16:00, 20:00
 #####################################################################
 
-batch4 <- processBatch(c(705168,705170,705169), "12:00", "No streaming",40)
-batch5 <- processBatch(c(705226,705228,705227), "16:00", "No streaming",40)
-batch6 <- processBatch(c(705241,705243,705242), "20:00", "No streaming",40)
+batch4 <- processBatch(c(705168,705170,705169), "12:00", "No streaming",40,to_download)
+batch5 <- processBatch(c(705226,705228,705227), "16:00", "No streaming",40,to_download)
+batch6 <- processBatch(c(705241,705243,705242), "20:00", "No streaming",40,to_download)
 
 batches_no_streaming <- rbind(batch4, batch5, batch6)
 #####################################################################
@@ -36,4 +37,4 @@ batches_all <- rbind(batches_with_streaming,batches_no_streaming)
 # PLOT GENERATION
 #####################################################################
 
-createPlot(batches_all,"plots/streaming_comparison_transposition.pdf",16,4, task~streaming )
+createPlot(batches_all,"example.pdf",16,9, task~streaming )
