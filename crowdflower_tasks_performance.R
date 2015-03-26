28adb7276bcedcc285120246e08c47e5268a553a
Pavel Kucherbaev <pavel.kucherbaev@gmail.com>
2015-03-26 16:12:45 +0100

    for no-streaming case - shift tasks 2 and 3 start right after task 1 is completed or 2 hours are exceeded.

--- crowdflower_tasks_performance.R
+++ crowdflower_tasks_performance.R
@@ -13,22 +13,54 @@ library(ggplot2)
 library(scales)
 
 dumb_start_time <- as.POSIXct("03/19/2015 00:00:00", format='%m/%d/%Y %H:%M:%S')
+duration_limit <- as.POSIXct("03/19/2015 3:30:00", format='%m/%d/%Y %H:%M:%S') - as.POSIXct("03/19/2015 02:00:00", format='%m/%d/%Y %H:%M:%S')
+cbbPalette <- c("#000000", "#009E73", "#e79f00", "#9ad0f3", "#0072B2", "#D55E00", 
+    "#CC79A7", "#F0E442")
 
 createPlot <- function(data, filename, binwidth, width, height, faceting){
-	hist_plot <- ggplot(data, aes(x=X_relative_time,y=..count.., fill = batch))
+	hist_plot <- ggplot(data, aes(x=X_relative_time,y=..count.., fill = batch, string.x ="x"))  #+ scale_fill_brewer()
 	# plot histogram
-	hist_plot <- hist_plot + geom_histogram(binwidth=binwidth, position="identity", alpha = 0.6, color="black")
+	hist_plot <- hist_plot + geom_histogram(binwidth=binwidth, position="identity", alpha = 0.6, color="black", drop = TRUE)
 	# plot labels
-	hist_plot <- hist_plot + xlab("Time") + ylab("Tasks completed") #+ ggtitle("Streaming") 
-	hist_plot <- hist_plot + scale_x_datetime(breaks = date_breaks("20 min"), minor_breaks = date_breaks("10 min"),labels = date_format("%H:%M"))
+	hist_plot <- hist_plot + xlab("Time") + ylab("Tasks instances completed") #+ ggtitle("Streaming") 
+	hist_plot <- hist_plot + scale_x_datetime(breaks = date_breaks("30 min"), minor_breaks = date_breaks("15 min"),labels = date_format("%H:%M")) #
 	# split into facets
 	hist_plot <- hist_plot + facet_grid(faceting)
 	# font size, theme settings
-	hist_plot <- hist_plot + theme(text = element_text(size=12, color = "black"), axis.text = element_text(size=12, color = "black"), title = element_text(size=16),strip.text = element_text(size = 16), strip.text.y = element_text(angle = 0),axis.title.y=element_text(vjust=1.5)) 
+	hist_plot <- hist_plot + theme(text = element_text(size=14, color = "black"), axis.text = element_text(size=14, color = "black"), title = element_text(size=18),strip.text = element_text(size = 18), strip.text.y = element_text(angle = 0,hjust = 0),axis.title.y=element_text(vjust=1.5)) + theme(legend.position="top")
 	# task end vertical lines
-	hist_plot <- hist_plot+ geom_vline(aes(xintercept=fin, colour = batch), data)
+	hist_plot <- hist_plot+ geom_vline(aes(xintercept=fin_relative_numeric, fill = batch, color = batch), data, alpha = 0.9, size = 0.6)#+ scale_colour_brewer()
 	# save the plot into the file
 	ggsave(hist_plot, file=filename, width=width, height=height)
+}
+defineEnd <- function(data, amount){
+	rows_amount <- nrow(data)
+	#end <- dumb_start_time + (min(data$X_created_at) - min(data$min_created_at))+duration_limit
+	end <- min(data$X_created_at)+ duration_limit
+	if (rows_amount >= amount){
+		data<- data[1:amount,]
+		if (data[amount,"X_created_at"] <= end){
+			end <- data[amount,"X_created_at"]
+			#print(data[1,"job_id"])
+			#print(end)
+			#print(data[1,"streaming"])
+			#print(data[1,"task"])
+			#print(data$X_created_at)
+		}
+	}
+	data['fin']<-end	
+	data["fin_relative"] <- dumb_start_time + (end - min(data$min_created_at))
+	data["fin_relative_numeric"] <- as.numeric(dumb_start_time + (end - min(data$min_created_at)))
+	
+	print(data[1,"job_id"])
+	print(data[1,"streaming"])
+	print(data[1,"task"])
+	print(data[1,"min_created_at"])
+	print(data[1,"fin"])
+	print(data[1,"fin_relative"])
+	
+	
+	data
 }
 getJobResults <- function(job_id, title, batch, streaming, subtract, scope, download = F){
 
@@ -42,10 +74,45 @@ getJobResults <- function(job_id, title, batch, streaming, subtract, scope, down
 
 	# parse string/factor columns into date format
 	data$X_created_at <- mdy_hms(data$X_created_at)
+	
+	data['job_id'] <- job_id
+	data["task"] <- title
+	data["batch"] <- batch
+	data["streaming"] <- streaming
+
+	data <- data[with(data, order(X_created_at)), ]
+	# BUG FIX (removal of extra judgements which came by mistake)
+	if (job_id=="704733"){
+		data<-data[-c(5,20), ]
+	}
+	data <- data[data$X_created_at < min(data$X_created_at)+duration_limit,]
+	
+
+	# ------------------------------------------
+	if (subtract){
+		data$min_created_at <- subtract
+	}else{
+		data$min_created_at <- min(data$X_created_at)
+	}
+
+	# set the end time
+	# ------------------------------------------
+	data$X_relative_time <- dumb_start_time + (data$X_created_at - data$min_created_at)
+
+	if (title == "1. Transcribe"){
+		data <- defineEnd(data,40)
+	}
+	if (title == "2. Check and fix"){
+		data <- defineEnd(data,20)
+	}
+	if (title == "3. Classify receipt"){
+		data <- defineEnd(data,10)
+	}
+	
 	# remove outliers
 	# ------------------------------------------
 	if(scope != 40) {
-		data <- data[with(data, order(X_created_at)), ]
+		
 		if (title == "1. Transcribe"){
 			data <- data[1:scope,]
 		}
@@ -56,23 +123,9 @@ getJobResults <- function(job_id, title, batch, streaming, subtract, scope, down
 			data <- data[1:(scope/4),]
 		}
 	}
-	# ------------------------------------------
-	if (subtract){
-		data$min_created_at <- subtract
-	}else{
-		data$min_created_at <- min(data$X_created_at)
-	}
 
-	data$X_relative_time <- dumb_start_time + (data$X_created_at - data$min_created_at)
-	data$X_started_at <- mdy_hms(data$X_started_at)
 	
-	data["task"] <- title
-	end <- max(data$X_relative_time)
-	data["fin"] <- as.numeric(end)
-	data["batch"] <- batch
-	data["streaming"] <- streaming
-	
-	subset(data, select=c("task","batch","streaming", "fin", "X_created_at","X_started_at","X_relative_time"))
+	subset(data, select=c("task","batch","streaming", "fin","fin_relative","fin_relative_numeric", "X_created_at","X_relative_time"))
 }
 
 processBatch <- function(batch_tasks, batch_name, condition, scope, download = F){
@@ -88,8 +141,8 @@ processBatch <- function(batch_tasks, batch_name, condition, scope, download = F
 		test1 <- getJobResults(batch_tasks[2], task_names[2], batch_name, condition, min(task1$X_created_at),scope, download)
 		test2 <- getJobResults(batch_tasks[3], task_names[3], batch_name, condition, min(task1$X_created_at),scope, download)
 		
-		task2 <- getJobResults(batch_tasks[2], task_names[2], batch_name, condition, min(task1$X_created_at)+(min(test1$X_created_at)-max(task1$X_created_at)),scope, download)
-		task3 <- getJobResults(batch_tasks[3], task_names[3], batch_name, condition, min(task1$X_created_at)+(min(test2$X_created_at)-max(task1$X_created_at)),scope, download)
+		task2 <- getJobResults(batch_tasks[2], task_names[2], batch_name, condition, min(task1$X_created_at)+(min(test1$X_created_at)-max(task1$fin)),scope, download)
+		task3 <- getJobResults(batch_tasks[3], task_names[3], batch_name, condition, min(task1$X_created_at)+(min(test2$X_created_at)-max(task1$fin)),scope, download)
 	}
 	batch <- rbind(task1,task2,task3)
 	batch
